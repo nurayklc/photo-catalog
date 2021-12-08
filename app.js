b94ec39e28bf1a2180af41b1cc198d41d8797620
nurayklc <nuriskl72@gmail.com>
2021-12-08 19:39:09 +0300

    Add nodemon and express.

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const func = function () {
-  console.log('Function Working');
-};
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    const photo = {
+        id : 1,
+        name : "Photo Name",
+        description : "Photo description"
+    }
+  res.send(photo);
+  res.end()
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu port ${port} da çalışıyor.`);
+});
