46af7aca15c65788f4b3fbfd5d998affd8cd5ef7
nurayklc <nuriskl72@gmail.com>
2021-12-14 17:26:11 +0300

    Complete MVC model.

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
 const methodOverride = require('method-override');
 const ejs = require('ejs');
-const fs = require('fs');
-const Photo = require('./models/Photo');
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageControllers');
 
 const app = express();
 
@@ -19,67 +19,22 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true })); // urlden gelen dataları alır.
 app.use(express.json()); // dataları json yapısına çevirir.
 app.use(fileUpload());
-app.use(methodOverride('_method',{
-  methods:['POST', 'GET']
-}));
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 // ROUTERING
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({}).sort('-dateCreated');
-  res.render('index', {
-    photos,
-  });
-});
-app.get('/photo/:id', async (req, res) => {
-  const photo = await Photo.findById(req.params.id);
-  res.render('photo', {
-    photo,
-  });
-});
-app.get('/photo/edit/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  res.render('edit', {
-    photo,
-  });
-});
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-app.get('/addPhoto', (req, res) => {
-  res.render('addPhoto');
-});
+app.get('/', photoController.getAllPhotos);
+app.get('/photo/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photo/:id', photoController.updatePhoto);
+app.delete('/photo/:id', photoController.deletePhoto);
 
-app.post('/photos', async (req, res) => {
-  //await Photo.create(req.body)
-  const uploadDir = 'public/uploads';
-  if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
-  let uploadedImage = req.files.image;
-  let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name;
-
-  uploadedImage.mv(uploadPath, async () => {
-    await Photo.create({
-      ...req.body,
-      image: '/uploads/' + uploadedImage.name,
-    });
-  });
-  res.redirect('/');
-});
-
-app.put('/photo/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  photo.title = req.body.title;
-  photo.description = req.body.description;
-  photo.save();
-  res.redirect(`/photo/${req.params.id}`);
-});
-
-app.delete('/photo/:id', async (req, res) => {
-  const photo = await Photo.findOne({ _id: req.params.id });
-  let deletedImage = __dirname +'/public'+photo.image
-  fs.unlinkSync(deletedImage)
-  await Photo.findByIdAndDelete(req.params.id)
-  res.redirect('/');
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/addPhoto', pageController.getAddPage);
+app.get('/photo/edit/:id', pageController.getEditPage);
 
 const port = 3000;
 
