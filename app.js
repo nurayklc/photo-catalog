4f349e9d57d53f76bbcaa13605de67e08857b635
nurayklc <nuriskl72@gmail.com>
2021-12-14 13:18:52 +0300

    Complete single pages.

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ app.get('/', async (req, res) => {
     photos
   });
 });
+app.get('/photo/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id)
+  res.render('photo',{
+    photo
+  });
+});
+
 app.get('/about', (req, res) => {
   res.render('about');
 });
