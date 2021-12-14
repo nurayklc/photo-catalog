8a540771fc9024e6fb312afd2e646d96d9d875b8
nurayklc <nuriskl72@gmail.com>
2021-12-14 14:09:11 +0300

    Complete upload image lesson.

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const fs = require('fs');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs');
-const Photo = require('./models/Photo')
+const Photo = require('./models/Photo');
 
 const app = express();
 
 // Connect Db
-mongoose.connect('mongodb://localhost/photo-catalog')
+mongoose.connect('mongodb://localhost/photo-catalog');
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 // MIDDLEWARES
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true})) // urlden gelen dataları alır.
-app.use(express.json()) // dataları json yapısına çevirir.
+app.use(express.urlencoded({ extended: true })); // urlden gelen dataları alır.
+app.use(express.json()); // dataları json yapısına çevirir.
+app.use(fileUpload());
 
 // ROUTERING
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({})
+  const photos = await Photo.find({});
   res.render('index', {
-    photos
+    photos,
   });
 });
 app.get('/photo/:id', async (req, res) => {
-  const photo = await Photo.findById(req.params.id)
-  res.render('photo',{
-    photo
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
   });
 });
 
@@ -38,7 +41,18 @@ app.get('/addPhoto', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body)
+  //await Photo.create(req.body)
+  const uploadDir = 'public/uploads';
+  if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
+  let uploadedImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name;
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadedImage.name,
+    });
+  });
   res.redirect('/');
 });
 
